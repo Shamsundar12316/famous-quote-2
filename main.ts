1cd13aa6b139ad660c74fae726fef5b90bf27556
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 02:11:54 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+let quote: string = "A person who never make a mistake never tried anything new";
+
+
+let famous_person: string = "Albert Einstein";
+let message: string = `${famous_person} once said, ${quote}`;
+console.log(message);
